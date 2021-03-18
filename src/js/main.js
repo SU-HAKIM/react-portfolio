68dc2a9b05315490aaffc9756016aea57dbe83fa
SU Hakim <bmsuhakim@gmail.com>
2021-03-18 23:27:38 +0600

    learning js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-console.log("sadi")
+let name = 'sadiul hakim';
+let age = 17;
+let language = 'Javascript';
+
+
+console.log(`${name} is ${age} years old and he is learning ${language}.He is ${age > 18 ? 'Adult' : 'Not Adult'}.`);
+
+let arr = [1, 2, 3, 4]
+console.log(arr.includes(12))
+console.log(name.padStart(15))
+console.log(name.padEnd(15, 'a'))
+console.log('S'.repeat(10))
